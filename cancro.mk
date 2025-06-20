fa740ffc5160b7cc7bb4ac050f081d757cab564a
roblamda <72200197+roblamda@users.noreply.github.com>
2025-06-20 15:38:50 +0200

    cancro: remove nfc entirely

--- cancro.mk
+++ cancro.mk
@@ -36,20 +36,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.qcom.fm.sh
 
-# NFC
-PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.0-impl-bcm \
-    nfc_nci.bcm2079x.default \
-    NfcNci \
-    Tag
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/vendor/etc/permissions/android.hardware.nfc.hce.xml \
-    frameworks/native/data/etc/android.hardware.nfc.xml:system/vendor/etc/permissions/android.hardware.nfc.xml \
-    $(DEVICE_PATH)/nfc/libnfc-nci.conf:system/etc/libnfc-nci.conf \
-    $(DEVICE_PATH)/nfc/libnfc-nci-20791b05.conf:system/etc/libnfc-nci-20791b05.conf \
-    $(DEVICE_PATH)/nfc/nfcee_access_debug.xml:system/etc/nfcee_access.xml
-
 # IR
 PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-service.cancro
